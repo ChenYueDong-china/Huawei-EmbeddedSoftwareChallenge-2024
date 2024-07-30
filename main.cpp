1b0459e16e2d459e2c0eb562bd5dc42f1129a716
chenyuedong <1051265545@qq.com>
2024-07-30 20:16:47 +0800

    增加astar2

--- main.cpp
+++ main.cpp
@@ -352,7 +352,189 @@ struct Strategy {
             }
             return path;
         }
+        inline static vector<Point> aStar2(const int start, const int end, const int width,
+                                           const vector<NearEdge> searchGraph[MAX_N + 1],
+                                           const vector<Edge> &edges, const vector<Vertex> &vertices,
+                                           const int minDistance[MAX_N + 1][MAX_N + 1], const int maxResource) {
+            static bitset<MAX_N + 1> parentVertexes[MAX_N + 1][CHANNEL_COUNT + 1];
+            struct FastQueue {
+                int size = 0;
+                int dataDist[CHANNEL_COUNT * MAX_N * 10]{};
+                int dataChannelVertex[CHANNEL_COUNT * MAX_N * 10]{};
 
+                void push(int dist, int channelVertex) {
+                    //上推
+                    int index = size + 1;
+                    while (index != 1) {
+                        int last = index >> 1;
+                        //大通道在前面会更高分
+                        if (dataDist[last] >= dist) {
+                            //下推；
+                            dataDist[index] = dataDist[last];
+                            dataChannelVertex[index] = dataChannelVertex[last];
+                            index >>= 1;
+                        } else {
+                            break;
+                        }
+                    }
+                    dataDist[index] = dist;
+                    dataChannelVertex[index] = channelVertex;
+                    ++size;
+                }
+
+                int pop() {
+                    //弹出第一个
+                    int result = dataChannelVertex[1];
+                    //最后一个放入第一个当中，下推
+                    int curDist = dataDist[size];
+                    int curChannelVertex = dataChannelVertex[size];
+                    int index = 1;
+                    while (true) {
+                        int next = index << 1;
+                        if (next >= size) break;
+                        int curMinDist = dataDist[next];
+                        int curMin = next;
+                        if (next + 1 < size && dataDist[next + 1] < curMinDist) {
+                            curMinDist = dataDist[next + 1];
+                            curMin = next + 1;
+                        }
+                        if (curMinDist < curDist) {
+                            dataDist[index] = dataDist[curMin];
+                            dataChannelVertex[index] = dataChannelVertex[curMin];
+                            index = curMin;
+                        } else {
+                            break;
+                        }
+                    }
+                    dataDist[index] = curDist;
+                    dataChannelVertex[index] = curChannelVertex;
+                    --size;
+                    return result;
+                }
+
+                bool empty() const {
+                    return size == 0;
+                }
+
+                void clear() {
+                    size = 0;
+                }
+            };
+            static int timestamp[MAX_N + 1][CHANNEL_COUNT + 1], dist[MAX_N + 1][CHANNEL_COUNT + 1]
+            , parentStartChannelEdgeId[MAX_N + 1][CHANNEL_COUNT + 1];
+            static int timestampId = 1;//距离
+            static FastQueue q;
+            q.clear();
+            timestampId++;
+            //往上丢是最好的，因为测试用例都往下丢，往上能流出更多空间
+            for (int i = 1; i <= CHANNEL_COUNT; ++i) {
+                dist[start][i] = 0;
+                timestamp[start][i] = timestampId;
+                parentVertexes[start][i].reset();
+                q.push(minDistance[start][end] * EDGE_LENGTH_WEIGHT * width, (i << 16) + start);
+            }
+            int endChannel = -1;
+            while (!q.empty()) {
+                const int poll = q.pop();
+                const int lastChannel = poll >> 16;
+                const int lastVertex = poll & 0xFFFF;
+                const int lastDeep = dist[lastVertex][lastChannel];
+                if (lastVertex == end) {
+                    endChannel = lastChannel;
+                    break;
+                }
+                for (const NearEdge &nearEdge: searchGraph[lastVertex]) {
+                    const int next = nearEdge.to;
+                    if (parentVertexes[lastVertex][lastChannel].test(next)) {
+                        //防止重边
+                        continue;
+                    }
+                    const Edge &edge = edges[nearEdge.id];
+                    if (edge.die) {
+                        continue;
+                    }
+                    if (lastVertex == start
+                        || vertices[lastVertex].curChangeCount <= 0
+                        || vertices[lastVertex].die) {
+                        //没法变通道
+                        if (!edge.widthChannelTable[width * (CHANNEL_COUNT + 1) + lastChannel]) {
+                            continue;//不空闲直接结束
+                        }
+                        const int startChannel = lastChannel;
+                        int nextDistance = lastDeep + width * EDGE_LENGTH_WEIGHT;//不用变通道
+                        if (timestamp[next][startChannel] == timestampId &&
+                            dist[next][startChannel] <= nextDistance) {
+                            //访问过了，且距离没变得更近
+                            continue;
+                        }
+                        if (nextDistance + width * EDGE_LENGTH_WEIGHT * minDistance[next][end] > maxResource) {
+                            continue;
+                        }
+                        timestamp[next][startChannel] = timestampId;
+                        dist[next][startChannel] = nextDistance;
+                        parentStartChannelEdgeId[next][startChannel] = (lastChannel << 16) + nearEdge.id;
+                        parentVertexes[next][startChannel] = parentVertexes[lastVertex][lastChannel];
+                        parentVertexes[next][startChannel].set(lastVertex);
+                        q.push(nextDistance + width * EDGE_LENGTH_WEIGHT * minDistance[next][end],
+                               (startChannel << 16) + next);
+                    } else {
+                        //能变通道
+                        const int *freeChannelTable = edge.freeChannelTable[width];
+                        for (int i = 1; i <= freeChannelTable[0]; ++i) {
+                            const int startChannel = freeChannelTable[i];
+                            //用来穷举
+                            int nextDistance = lastDeep + width * EDGE_LENGTH_WEIGHT;
+                            if (startChannel != lastChannel) {
+                                nextDistance += CHANGE_CHANNEL_WEIGHT;//变通道距离加1
+                            }
+                            if (timestamp[next][startChannel] == timestampId &&
+                                dist[next][startChannel] <= nextDistance) {
+                                //访问过了，且距离没变得更近
+                                continue;
+                            }
+                            if (nextDistance + width * EDGE_LENGTH_WEIGHT * minDistance[next][end] > maxResource) {
+                                continue;
+                            }
+                            timestamp[next][startChannel] = timestampId;
+                            dist[next][startChannel] = nextDistance;
+                            parentStartChannelEdgeId[next][startChannel] = (lastChannel << 16) + nearEdge.id;
+                            parentVertexes[next][startChannel] = parentVertexes[lastVertex][lastChannel];
+                            parentVertexes[next][startChannel].set(lastVertex);
+                            q.push(nextDistance + width * EDGE_LENGTH_WEIGHT * minDistance[next][end],
+                                   (startChannel << 16) + next);
+                        }
+                    }
+                }
+            }
+            if (endChannel == -1) {
+                return {};
+            }
+            vector<Point> path;
+            int cur = end;
+            int curStartChannel = endChannel;
+            while (cur != start) {
+                int edgeId = (parentStartChannelEdgeId[cur][curStartChannel] & 0xFFFF);
+                path.push_back({edgeId, curStartChannel, curStartChannel + width - 1});
+                int startChannel = (parentStartChannelEdgeId[cur][curStartChannel] >> 16);
+                cur = edges[edgeId].from == cur ? edges[edgeId].to : edges[edgeId].from;
+                curStartChannel = startChannel;
+            }
+            reverse(path.begin(), path.end());
+            int from = start;
+            unordered_set<int> keys;
+            keys.insert(from);
+            for (const Point &point: path) {
+                const Edge &edge = edges[point.edgeId];
+                int to = edge.from == from ? edge.to : edge.from;
+                if (keys.count(to)) {
+                    //顶点重复，锁死这个出边和下一个顶点好一点
+                    return {};
+                }
+                keys.insert(to);
+                from = to;
+            }
+            return path;
+        }
     };
 
     //恢复场景
